75471d3268086c4735981469f9f9f36fdcb4204b
Jigyasa <jigyasa@jirehsol.com>
2025-02-16 13:31:25 +0530

    .gdg\

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@
 
 
 const express = require("express");
-const mongoose = require("mongoose");
+
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -12,19 +12,10 @@ const app = express();
 
 
 
-// Router instance
-const router = express.Router(); 
-
 
 // Routes
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
-
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => console.log("Connected to DB"))
-  .catch((err) => console.error("DB Connection Error:", err));
 
 
 
@@ -32,7 +23,7 @@ mongoose
 
 
 // Starting server
-const PORT = process.env.PORT || 3001;
+const PORT =  3001;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}!`);
 });
