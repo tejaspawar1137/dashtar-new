0984d75b35249a4d92b5b2b41b72c0cd87f100ab
Jigyasa <jigyasa@jirehsol.com>
2025-02-16 13:43:52 +0530

    gdg

--- routes/templetes.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const templeteController = require('../controllers/templetes.controllers');
-
-const router = express.Router();
-
-router.post('/createTemplete', templeteController.sendMediaMessage);
-// router.put('/updateStatus', requestController.updateRequest);
-// router.get('/getRideRequest', requestController.getRequest);
-module.exports = router;
